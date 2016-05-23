82af1c38cb1572eabe92baf258ec1b80a9c5ae0f
romero.silva <romero.ca.silva@gmail.com>
2016-05-22 23:30:51 -0300

    Added page layout

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +27,22 @@ namespace MeetechApp
         public MainPage()
         {
             this.InitializeComponent();
+            var titleBar = ApplicationView.GetForCurrentView().TitleBar;
+            var bgColor = new Color
+            {
+                R = 97,
+                G = 26,
+                B = 119
+            };
+            titleBar.BackgroundColor = bgColor;
+            titleBar.ForegroundColor = Colors.White;
+            titleBar.ButtonBackgroundColor = bgColor;
+            titleBar.ButtonForegroundColor = Colors.White;
+        }
+
+        private void HamburgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            Menu.IsPaneOpen = !Menu.IsPaneOpen;
         }
     }
 }
